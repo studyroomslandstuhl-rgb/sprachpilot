3f506c51dc1a03689e67ef9ec5b406aa954a1a3b
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-15 15:59:05 +0200

    Update verbs.js

--- verben-A1/data/verbs.js
+++ verben-A1/data/verbs.js
@@ -1,1 +1,215 @@
+// verbs.js
 
+export const VERBS = {
+
+  lieben: {
+    image: "bilder/lieben.png",
+    level: "A1",
+    topic: "Basis",
+    irregular: false
+  },
+
+  kaufen: {
+    image: "bilder/kaufen.png",
+    level: "A1",
+    topic: "Einkaufen",
+    irregular: false
+  },
+
+  verstehen: {
+    image: "bilder/verstehen.png",
+    level: "A1",
+    topic: "Kurs",
+    irregular: false
+  },
+
+  sein: {
+    image: "bilder/sein.png",
+    level: "A1",
+    topic: "Basis",
+    irregular: true
+  },
+
+  haben: {
+    image: "bilder/haben.png",
+    level: "A1",
+    topic: "Basis",
+    irregular: true
+  },
+
+  gehen: {
+    image: "bilder/gehen.png",
+    level: "A1",
+    topic: "Bewegung",
+    irregular: true
+  },
+
+  essen: {
+    image: "bilder/essen.png",
+    level: "A1",
+    topic: "Essen",
+    irregular: true
+  },
+
+  sprechen: {
+    image: "bilder/sprechen.png",
+    level: "A1",
+    topic: "Kommunikation",
+    irregular: true
+  },
+
+  fahren: {
+    image: "bilder/fahren.png",
+    level: "A1",
+    topic: "Verkehr",
+    irregular: true
+  },
+
+  schlafen: {
+    image: "bilder/schlafen.png",
+    level: "A1",
+    topic: "Alltag",
+    irregular: true
+  },
+
+  sehen: {
+    image: "bilder/sehen.png",
+    level: "A1",
+    topic: "Wahrnehmung",
+    irregular: true
+  },
+
+  lesen: {
+    image: "bilder/lesen.png",
+    level: "A1",
+    topic: "Kurs",
+    irregular: true
+  },
+
+  rufen: {
+    image: "bilder/rufen.png",
+    level: "A1",
+    topic: "Kommunikation",
+    irregular: true
+  },
+
+  schreien: {
+    image: "bilder/schreien.png",
+    level: "A1",
+    topic: "Kommunikation",
+    irregular: false
+  },
+
+  träumen: {
+    image: "bilder/traeumen.png",
+    level: "A1",
+    topic: "Alltag",
+    irregular: false
+  },
+
+  kleben: {
+    image: "bilder/kleben.png",
+    level: "A1",
+    topic: "Kurs",
+    irregular: false
+  },
+
+  kosten: {
+    image: "bilder/kosten.png",
+    level: "A1",
+    topic: "Einkaufen",
+    irregular: false
+  },
+
+  schieben: {
+    image: "bilder/schieben.png",
+    level: "A1",
+    topic: "Bewegung",
+    irregular: true
+  },
+
+  ziehen: {
+    image: "bilder/ziehen.png",
+    level: "A1",
+    topic: "Bewegung",
+    irregular: true
+  },
+
+  schenken: {
+    image: "bilder/schenken.png",
+    level: "A1",
+    topic: "Familie",
+    irregular: false
+  },
+
+  füttern: {
+    image: "bilder/fuettern.png",
+    level: "A1",
+    topic: "Tiere",
+    irregular: false
+  },
+
+  kontrollieren: {
+    image: "bilder/kontrollieren.png",
+    level: "A1",
+    topic: "Kurs",
+    irregular: false
+  },
+
+  funktionieren: {
+    image: "bilder/funktionieren.png",
+    level: "A1",
+    topic: "Technik",
+    irregular: false
+  },
+
+  stehen: {
+    image: "bilder/stehen.png",
+    level: "A1",
+    topic: "Position",
+    irregular: true
+  },
+
+  bekommen: {
+    image: "bilder/bekommen.png",
+    level: "A1",
+    topic: "Alltag",
+    irregular: true
+  },
+
+  schmecken: {
+    image: "bilder/schmecken.png",
+    level: "A1",
+    topic: "Essen",
+    irregular: false
+  },
+
+  bezahlen: {
+    image: "bilder/bezahlen.png",
+    level: "A1",
+    topic: "Einkaufen",
+    irregular: false
+  },
+
+  empfehlen: {
+    image: "bilder/empfehlen.png",
+    level: "A1",
+    topic: "Kommunikation",
+    irregular: true
+  },
+
+  geben: {
+    image: "bilder/geben.png",
+    level: "A1",
+    topic: "Alltag",
+    irregular: true
+  },
+
+  backen: {
+    image: "bilder/backen.png",
+    level: "A1",
+    topic: "Kochen",
+    irregular: true
+  }
+
+};
