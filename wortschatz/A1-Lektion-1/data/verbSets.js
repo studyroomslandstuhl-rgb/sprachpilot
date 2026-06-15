52cd97302565e4c859f1c9115ddf581752f10eda
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-15 21:00:56 +0200

    Add files via upload

---
+++ wortschatz/A1-Lektion-1/data/verbSets.js
@@ -0,0 +1,291 @@
+export const VERB_SETS = {
+  "a1-standard-start": {
+    "title": "A1 Standard Start",
+    "level": "A1",
+    "verbs": [
+      "lieben",
+      "kaufen",
+      "verstehen",
+      "brauchen",
+      "hören",
+      "lernen",
+      "wohnen",
+      "bringen",
+      "sein",
+      "schreiben",
+      "fotografieren",
+      "telefonieren",
+      "kochen",
+      "leben",
+      "kommen",
+      "buchstabieren",
+      "gehen",
+      "schwimmen",
+      "suchen",
+      "bestellen"
+    ],
+    "tasks": [
+      "flashcards",
+      "memory",
+      "imageToVerb",
+      "verbToImage",
+      "writeVerb",
+      "hearWrite",
+      "hearSpeak",
+      "imageSpeak",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-1": {
+    "title": "A1 Erweiterung 1",
+    "level": "A1",
+    "verbs": [
+      "weinen",
+      "reparieren",
+      "gewinnen",
+      "spielen",
+      "springen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-2": {
+    "title": "A1 Erweiterung 2",
+    "level": "A1",
+    "verbs": [
+      "verlieren",
+      "fragen",
+      "verkaufen",
+      "unterschreiben",
+      "reservieren"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-3": {
+    "title": "A1 Erweiterung 3",
+    "level": "A1",
+    "verbs": [
+      "buchen",
+      "machen",
+      "malen",
+      "trinken",
+      "schicken"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-4": {
+    "title": "A1 Erweiterung 4",
+    "level": "A1",
+    "verbs": [
+      "denken",
+      "winken",
+      "hassen",
+      "beißen",
+      "gießen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-5": {
+    "title": "A1 Erweiterung 5",
+    "level": "A1",
+    "verbs": [
+      "reißen",
+      "putzen",
+      "küssen",
+      "tanzen",
+      "mixen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-6": {
+    "title": "A1 Erweiterung 6",
+    "level": "A1",
+    "verbs": [
+      "reisen",
+      "haben",
+      "überqueren",
+      "grüßen",
+      "genießen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-7": {
+    "title": "A1 Erweiterung 7",
+    "level": "A1",
+    "verbs": [
+      "singen",
+      "schließen",
+      "rennen",
+      "lachen",
+      "warten"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-8": {
+    "title": "A1 Erweiterung 8",
+    "level": "A1",
+    "verbs": [
+      "arbeiten",
+      "finden",
+      "benutzen",
+      "rechnen",
+      "zeichnen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-9": {
+    "title": "A1 Erweiterung 9",
+    "level": "A1",
+    "verbs": [
+      "öffnen",
+      "wiederholen",
+      "schneiden",
+      "streiten",
+      "atmen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-10": {
+    "title": "A1 Erweiterung 10",
+    "level": "A1",
+    "verbs": [
+      "essen",
+      "sprechen",
+      "fahren",
+      "schlafen",
+      "sehen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-11": {
+    "title": "A1 Erweiterung 11",
+    "level": "A1",
+    "verbs": [
+      "lesen",
+      "rufen",
+      "schreien",
+      "träumen",
+      "kleben"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-12": {
+    "title": "A1 Erweiterung 12",
+    "level": "A1",
+    "verbs": [
+      "kosten",
+      "schieben",
+      "ziehen",
+      "schenken",
+      "füttern"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-13": {
+    "title": "A1 Erweiterung 13",
+    "level": "A1",
+    "verbs": [
+      "kontrollieren",
+      "funktionieren",
+      "stehen",
+      "bekommen",
+      "schmecken"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  },
+  "a1-erweiterung-14": {
+    "title": "A1 Erweiterung 14",
+    "level": "A1",
+    "verbs": [
+      "bezahlen",
+      "empfehlen",
+      "geben",
+      "backen"
+    ],
+    "tasks": [
+      "flashcards",
+      "imageToVerb",
+      "writeVerb",
+      "sentencePuzzle"
+    ],
+    "unlockMode": "teacher"
+  }
+};
