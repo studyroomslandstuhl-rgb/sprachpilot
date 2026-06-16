8cd090bf1cd1a6ec51d6486ebd236260a9265879
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-16 09:06:40 +0200

    Delete wortschatz/A1-Lektion-1/shared directory

--- wortschatz/A1-Lektion-1/shared/ui.js
+++
@@ -1,4 +0,0 @@
-export function progressHtml(percent){
-  return `<div class="progress-line"><div class="progress-fill" style="width:${percent}%"></div></div>`;
-}
-export function notice(type,msg){ return `<div class="${type}">${msg}</div>`; }
