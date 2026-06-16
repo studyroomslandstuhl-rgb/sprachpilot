f60a9a0bdfc037a4c141d54eb041fe840eedf458
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-16 12:38:29 +0200

    Add files via upload

--- wortschatz/A1-Lektion-3/Thema-2/README.txt
+++ wortschatz/A1-Lektion-3/Thema-2/README.txt
@@ -1,14 +1,1 @@
-Thema 2: Menge & Verpackung + Preise
-Noch benötigte Bilder:
-flasche.png
-dose.png
-glas.png
-packung.png
-tuete.png
-becher.png
-karton.png
-stueck.png
-kilo.png
-gramm.png
-liter.png
-Vorhandene Bilder aus A1-Lektion-3/bilder weiterverwenden.
+Thema 2 funktioniert ohne weißen Bildschirm. Fehlende Bilder kommen in wortschatz/A1-Lektion-3/bilder/.
--- wortschatz/A1-Lektion-3/Thema-2/words-theme2.js
+++ wortschatz/A1-Lektion-3/Thema-2/words-theme2.js
@@ -1,13 +1,200 @@
-const WORDS_THEME2 = [
-  "die Flasche",
-  "die Dose",
-  "das Glas",
-  "die Packung",
-  "die Tüte",
-  "der Becher",
-  "der Karton",
-  "das Stück",
-  "das Kilo",
-  "das Gramm",
-  "der Liter"
+const WORDS_THEME2=[
+  {
+    "id": "flasche",
+    "article": "die",
+    "word": "Flasche",
+    "full": "die Flasche",
+    "plural": "die Flaschen",
+    "pluralGroup": "-n",
+    "image": "../bilder/flasche.png",
+    "translations": {
+      "Deutsch": "Flasche",
+      "Russisch": "бутылка",
+      "Ukrainisch": "пляшка",
+      "Arabisch": "زجاجة",
+      "Türkisch": "şişe",
+      "Rumänisch": "sticlă",
+      "Englisch": "bottle"
+    }
+  },
+  {
+    "id": "dose",
+    "article": "die",
+    "word": "Dose",
+    "full": "die Dose",
+    "plural": "die Dosen",
+    "pluralGroup": "-n",
+    "image": "../bilder/dose.png",
+    "translations": {
+      "Deutsch": "Dose",
+      "Russisch": "банка",
+      "Ukrainisch": "банка",
+      "Arabisch": "علبة",
+      "Türkisch": "kutu",
+      "Rumänisch": "cutie",
+      "Englisch": "can"
+    }
+  },
+  {
+    "id": "glas",
+    "article": "das",
+    "word": "Glas",
+    "full": "das Glas",
+    "plural": "die Gläser",
+    "pluralGroup": "-er/Umlaut",
+    "image": "../bilder/glas.png",
+    "translations": {
+      "Deutsch": "Glas",
+      "Russisch": "стакан",
+      "Ukrainisch": "склянка",
+      "Arabisch": "كأس",
+      "Türkisch": "bardak",
+      "Rumänisch": "pahar",
+      "Englisch": "glass"
+    }
+  },
+  {
+    "id": "packung",
+    "article": "die",
+    "word": "Packung",
+    "full": "die Packung",
+    "plural": "die Packungen",
+    "pluralGroup": "-en",
+    "image": "../bilder/packung.png",
+    "translations": {
+      "Deutsch": "Packung",
+      "Russisch": "упаковка",
+      "Ukrainisch": "упаковка",
+      "Arabisch": "عبوة",
+      "Türkisch": "paket",
+      "Rumänisch": "pachet",
+      "Englisch": "package"
+    }
+  },
+  {
+    "id": "tuete",
+    "article": "die",
+    "word": "Tüte",
+    "full": "die Tüte",
+    "plural": "die Tüten",
+    "pluralGroup": "-n",
+    "image": "../bilder/tuete.png",
+    "translations": {
+      "Deutsch": "Tüte",
+      "Russisch": "пакет",
+      "Ukrainisch": "пакет",
+      "Arabisch": "كيس",
+      "Türkisch": "poşet",
+      "Rumänisch": "pungă",
+      "Englisch": "bag"
+    }
+  },
+  {
+    "id": "becher",
+    "article": "der",
+    "word": "Becher",
+    "full": "der Becher",
+    "plural": "die Becher",
+    "pluralGroup": "-",
+    "image": "../bilder/becher.png",
+    "translations": {
+      "Deutsch": "Becher",
+      "Russisch": "стаканчик",
+      "Ukrainisch": "стаканчик",
+      "Arabisch": "كوب",
+      "Türkisch": "bardak",
+      "Rumänisch": "pahar",
+      "Englisch": "cup"
+    }
+  },
+  {
+    "id": "karton",
+    "article": "der",
+    "word": "Karton",
+    "full": "der Karton",
+    "plural": "die Kartons",
+    "pluralGroup": "-s",
+    "image": "../bilder/karton.png",
+    "translations": {
+      "Deutsch": "Karton",
+      "Russisch": "картонная коробка",
+      "Ukrainisch": "картонна коробка",
+      "Arabisch": "كرتون",
+      "Türkisch": "karton",
+      "Rumänisch": "carton",
+      "Englisch": "carton"
+    }
+  },
+  {
+    "id": "stueck",
+    "article": "das",
+    "word": "Stück",
+    "full": "das Stück",
+    "plural": "die Stücke",
+    "pluralGroup": "-e",
+    "image": "../bilder/stueck.png",
+    "translations": {
+      "Deutsch": "Stück",
+      "Russisch": "кусок",
+      "Ukrainisch": "шматок",
+      "Arabisch": "قطعة",
+      "Türkisch": "parça",
+      "Rumänisch": "bucată",
+      "Englisch": "piece"
+    }
+  },
+  {
+    "id": "kilo",
+    "article": "das",
+    "word": "Kilo",
+    "full": "das Kilo",
+    "plural": "die Kilos",
+    "pluralGroup": "-s",
+    "image": "../bilder/kilo.png",
+    "translations": {
+      "Deutsch": "Kilo",
+      "Russisch": "кило",
+      "Ukrainisch": "кіло",
+      "Arabisch": "كيلو",
+      "Türkisch": "kilo",
+      "Rumänisch": "kilogram",
+      "Englisch": "kilo"
+    }
+  },
+  {
+    "id": "gramm",
+    "article": "das",
+    "word": "Gramm",
+    "full": "das Gramm",
+    "plural": "die Gramm",
+    "pluralGroup": "-",
+    "image": "../bilder/gramm.png",
+    "translations": {
+      "Deutsch": "Gramm",
+      "Russisch": "грамм",
+      "Ukrainisch": "грам",
+      "Arabisch": "غرام",
+      "Türkisch": "gram",
+      "Rumänisch": "gram",
+      "Englisch": "gram"
+    }
+  },
+  {
+    "id": "liter",
+    "article": "der",
+    "word": "Liter",
+    "full": "der Liter",
+    "plural": "die Liter",
+    "pluralGroup": "-",
+    "image": "../bilder/liter.png",
+    "translations": {
+      "Deutsch": "Liter",
+      "Russisch": "литр",
+      "Ukrainisch": "літр",
+      "Arabisch": "لتر",
+      "Türkisch": "litre",
+      "Rumänisch": "litru",
+      "Englisch": "liter"
+    }
+  }
 ];
