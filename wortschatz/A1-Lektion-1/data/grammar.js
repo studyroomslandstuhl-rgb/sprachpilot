56c96ab0391a6ce4b84192fc6370aa7018e11c44
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-17 21:55:03 +0200

    Add files via upload

---
+++ wortschatz/A1-Lektion-1/data/grammar.js
@@ -0,0 +1,6 @@
+// data/grammar.js
+// Besondere Formen, die in Grammatik-/Schreibaufgaben genutzt werden können.
+
+const IRREGULAR_VERB_FORMS = {"empfehlen":{"ich":"empfehle","du":"empfiehlst","er/sie/es":"empfiehlt"},"geben":{"ich":"gebe","du":"gibst","er/sie/es":"gibt"},"backen":{"ich":"backe","du":"bäckst","er/sie/es":"bäckt"}};
+
+if (typeof window !== "undefined") window.IRREGULAR_VERB_FORMS = IRREGULAR_VERB_FORMS;
