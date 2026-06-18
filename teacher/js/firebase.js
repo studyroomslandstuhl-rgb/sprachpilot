c3fdc41beb72ebba41a46aa7a229c7519915c5b6
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-18 13:16:39 +0200

    Update firebase.js

--- teacher/js/firebase.js
+++ teacher/js/firebase.js
@@ -1,1 +1,31 @@
-let db=null;try{if(firebase&&!firebase.apps.length)firebase.initializeApp({apiKey:"AIzaSyDbl0m8JIEu7BuoLwXrdxRL4wMJAVJS468",authDomain:"sprachpilot-12c68.firebaseapp.com",projectId:"sprachpilot-12c68",storageBucket:"sprachpilot-12c68.firebasestorage.app",messagingSenderId:"454992284519",appId:"1:454992284519:web:c7a87558cf59e0c0fc7dc2"});db=firebase.firestore()}catch(e){console.warn(e)}
+let db = null;
+let auth = null;
+
+try {
+
+  const firebaseConfig = {
+    apiKey: "DEIN_API_KEY",
+    authDomain: "sprachpilot-12c68.firebaseapp.com",
+    projectId: "sprachpilot-12c68",
+    storageBucket: "sprachpilot-12c68.firebasestorage.app",
+    messagingSenderId: "454992284519",
+    appId: "1:454992284519:web:c7a87558cf59e0c0fc7dc2"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  db = firebase.firestore();
+
+  if (firebase.auth) {
+    auth = firebase.auth();
+  }
+
+  console.log("Firebase verbunden");
+
+} catch (err) {
+
+  console.error("Firebase Fehler:", err);
+
+}
