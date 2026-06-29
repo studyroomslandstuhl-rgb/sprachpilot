92a26fec2dfdb7af823c7809726898ad7504fa4a
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-29 14:39:44 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, serverTimestamp, collection, query, where, getDocs, limit } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyDbl0m8JIEu7BuoLwXrdxRL4wMJAVJS468",
   authDomain: "sprachpilot-12c68.firebaseapp.com",
@@ -7,5 +9,6 @@ const firebaseConfig = {
   appId: "1:454992284519:web:c7a87558cf59e0c0fc7dc2",
   measurementId: "G-2XXR3FSY89"
 };
-if (window.firebase && !firebase.apps.length) firebase.initializeApp(firebaseConfig);
-const db = window.firebase ? firebase.firestore() : null;
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { doc, getDoc, setDoc, updateDoc, serverTimestamp, collection, query, where, getDocs, limit };
