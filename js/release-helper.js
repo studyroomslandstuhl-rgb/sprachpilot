f22c9eaa87a0e9ae98378ddfe7e73e093cac738e
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-30 14:30:44 +0200

    Bump topbar logo fix cache

--- js/release-helper.js
+++ js/release-helper.js
@@ -1,4 +1,4 @@
-import('/js/topbar-standard.js?v=1').catch(function(){});
+import('/js/topbar-standard.js?v=2').catch(function(){});
 (function(){
   let remoteData=null;
   function safeJson(s,f){try{return JSON.parse(s||"")||f}catch(e){return f}}
