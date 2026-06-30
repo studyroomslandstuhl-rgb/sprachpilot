4d2305601e39a594ca6af8e4effff75750b5239f
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-06-30 16:13:26 +0200

    Add vergeben verbringen to teacher verb releases

--- teacher/verb-release-extra.js
+++ teacher/verb-release-extra.js
@@ -1,6 +1,10 @@
 (function(){
   try{
+    if(window.ALL_VERBS){
+      const existing=new Set(window.ALL_VERBS.map(x=>x.v));
+      [{v:'vergeben',img:'vergeben'},{v:'verbringen',img:'verbringen'}].forEach(item=>{if(!existing.has(item.v)){window.ALL_VERBS.push(item);existing.add(item.v);}});
+    }
     if(typeof STRONG_IRREGULAR_VERBS==='undefined')return;
-    ['anfangen','beginnen','fernsehen','aussterben','begraben','verbiegen','mitgeben','mitnehmen'].forEach(v=>STRONG_IRREGULAR_VERBS.add(v));
+    ['anfangen','beginnen','fernsehen','aussterben','begraben','verbiegen','mitgeben','mitnehmen','vergeben'].forEach(v=>STRONG_IRREGULAR_VERBS.add(v));
   }catch(e){console.warn('Neue Verben konnten nicht klassifiziert werden',e)}
 })();
