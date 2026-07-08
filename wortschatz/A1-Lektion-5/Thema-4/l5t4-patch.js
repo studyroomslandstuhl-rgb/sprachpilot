74e78d064a354d14334490ac0478e9b30ae2e04a
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-08 09:22:11 +0200

    Patch L5T4 vocabulary aliases and images

---
+++ wortschatz/A1-Lektion-5/Thema-4/l5t4-patch.js
@@ -0,0 +1,11 @@
+(function(){
+  function findWord(id){return WORDS.find(w=>w&&w.id===id)}
+  function patch(id,data){const w=findWord(id);if(w)Object.assign(w,data)}
+  ['halb','kindergarten','jugendliche'].forEach(id=>{const idx=WORDS.findIndex(w=>w&&w.id===id);if(idx>=0)WORDS.splice(idx,1)})
+  patch('ist_geoeffnet',{image:null})
+  patch('ist_geschlossen',{image:null})
+  patch('zum_beispiel',{word:'z.B.',full:'z.B.',sentence:'z.B.: Die Kita ist geöffnet.'})
+  patch('kita',{full:'die Kita',word:'Kita',aliases:['die Kita','Kita','der Kindergarten','Kindergarten']})
+  patch('jugend',{full:'die Jugend / die Jugendlichen',word:'Jugend / Jugendliche',aliases:['die Jugend','Jugend','die Jugendlichen','Jugendliche','Jugendlichen']})
+  window.l5t4Accepted=function(txt,w){const a=simple(txt);return !!(w&&Array.isArray(w.aliases)&&w.aliases.some(x=>simple(x)===a))}
+})();
