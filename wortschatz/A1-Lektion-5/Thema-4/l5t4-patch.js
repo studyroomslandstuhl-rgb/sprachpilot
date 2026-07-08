5179704f09830e765e43bdafb21ce6a1252c41b3
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-08 09:25:20 +0200

    Refine L5T4 merged vocabulary entries

--- wortschatz/A1-Lektion-5/Thema-4/l5t4-patch.js
+++ wortschatz/A1-Lektion-5/Thema-4/l5t4-patch.js
@@ -5,7 +5,7 @@
   patch('ist_geoeffnet',{image:null})
   patch('ist_geschlossen',{image:null})
   patch('zum_beispiel',{word:'z.B.',full:'z.B.',sentence:'z.B.: Die Kita ist geöffnet.'})
-  patch('kita',{full:'die Kita',word:'Kita',aliases:['die Kita','Kita','der Kindergarten','Kindergarten']})
-  patch('jugend',{full:'die Jugend / die Jugendlichen',word:'Jugend / Jugendliche',aliases:['die Jugend','Jugend','die Jugendlichen','Jugendliche','Jugendlichen']})
+  patch('kita',{full:'die Kita / der Kindergarten',word:'Kita / Kindergarten',plural:'die Kitas / die Kindergärten',pluralGroup:'Pl.',aliases:['die Kita','Kita','der Kindergarten','Kindergarten']})
+  patch('jugend',{full:'die Jugend / die Jugendlichen',word:'Jugend / Jugendliche',plural:'die Jugendlichen',pluralGroup:'Pl.',aliases:['die Jugend','Jugend','die Jugendlichen','Jugendliche','Jugendlichen']})
   window.l5t4Accepted=function(txt,w){const a=simple(txt);return !!(w&&Array.isArray(w.aliases)&&w.aliases.some(x=>simple(x)===a))}
 })();
