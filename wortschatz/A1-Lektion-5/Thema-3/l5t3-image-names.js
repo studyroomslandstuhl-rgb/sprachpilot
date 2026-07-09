d5a804034751d1e7580e968cb6147b80bf2728c6
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-09 21:58:53 +0200

    Use Neu Bunny folder for L5T3 images

--- wortschatz/A1-Lektion-5/Thema-3/l5t3-image-names.js
+++ wortschatz/A1-Lektion-5/Thema-3/l5t3-image-names.js
@@ -1,12 +1,10 @@
 (function(){
-  const IMG={
-    morgen:'/assets/img/der_morgen.png',
-    vormittag:'/assets/img/der_vormittag.png',
-    mittag:'/assets/img/der_mittag.png',
-    nachmittag:'/assets/img/der_nachmittag.png',
-    abend:'/assets/img/der_abend.png',
-    nacht:'/assets/img/die_nacht.png',
-    mitternacht:'/assets/img/die_mitternacht.png'
-  };
-  if(typeof WORDS!=='undefined')WORDS.forEach(w=>{if(IMG[w.id])w.image=IMG[w.id]});
+  const BASE='https://sprachpilot.b-cdn.net/Neu/';
+  function cleanName(name){return String(name||'').split('?')[0].split('#')[0].split('/').pop().replace(/\.(webp|png|jpe?g|gif|svg)$/i,'')}
+  function norm(id){return cleanName(id).toLowerCase().replace(/ä/g,'ae').replace(/ö/g,'oe').replace(/ü/g,'ue').replace(/ß/g,'ss').replace(/[^a-z0-9]+/g,'_').replace(/^_+|_+$/g,'')}
+  function bunny(id){const n=norm(id);return n?BASE+n+'.webp':''}
+  if(typeof WORDS!=='undefined')WORDS.forEach(w=>{w.image=bunny(w.id||w.word)});
+  window.cdnImg=function(w){return bunny(w&&w.id||w&&w.image||w&&w.word)};
+  window.imgHtml=imgHtml=function(w){return `<img src="${cdnImg(w)}" onerror="fixImg(this,'${String(w?.word||'').replace(/'/g,'&#039;')}')" alt="">`};
+  window.bigImgHtml=bigImgHtml=function(w){return `<img class="task-img" src="${cdnImg(w)}" onerror="fixBigImg(this,'${String(w?.word||'').replace(/'/g,'&#039;')}')" alt="">`};
 })();
