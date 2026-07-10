819d880be6249c3b32adf6c59814eb29f421cdc1
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-10 10:54:48 +0200

    Bump Bunny asset loader in guard

--- js/guard.js
+++ js/guard.js
@@ -1,4 +1,4 @@
-import "/js/sp-assets.js?v=3";
+import "/js/sp-assets.js?v=4";
 import "/js/session-restore.js?v=1";
 import { requireLogin, renderAccountStrip, logout } from "/js/auth.js?v=step136-login-fix";
 
