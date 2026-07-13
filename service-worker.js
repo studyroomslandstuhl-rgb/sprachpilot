a82acaf22b309e3132af10685de10fcef2f66695
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-13 23:36:53 +0200

    Cache nach L3T2 Wiederholungsfix erneuern

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="sprachpilot-static-v20260713-l3t2-task-unblock-1";
+const CACHE_VERSION="sprachpilot-static-v20260713-l3t2-repeat-1";
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
