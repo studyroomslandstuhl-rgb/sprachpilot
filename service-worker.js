658b10ba4373ef73921c0836f25d9fed286784af
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-14 00:15:37 +0200

    Cache nach L6T2 Wiederholen Standard erneuern

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="sprachpilot-static-v20260713-l5t1-exam-lock-1";
+const CACHE_VERSION="sprachpilot-static-v20260714-l6t2-repeat-1";
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
