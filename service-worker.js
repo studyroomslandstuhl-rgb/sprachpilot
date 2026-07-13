691d3d0af63a7b764ed62e97fec078d16baaab09
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-14 00:33:00 +0200

    Cache nach L5T1 Renderfix aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="sprachpilot-static-v20260714-theme-overview-guardfix";
+const CACHE_VERSION="sprachpilot-static-v20260714-theme-overview-renderfix";
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
