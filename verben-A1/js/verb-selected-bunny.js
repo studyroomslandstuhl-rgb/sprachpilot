bdc732f6317c75cf29e351fd91202cffb917c167
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-14 08:29:59 +0200

    Verlinke Bunny-Bilder für 36 neue Verben

--- verben-A1/js/verb-selected-bunny.js
+++ verben-A1/js/verb-selected-bunny.js
@@ -1,4 +1,5 @@
 (function(){
+  const BASE='https://sprachpilot.b-cdn.net/';
   const MAP={
     'anfangen':'https://sprachpilot.b-cdn.net/Neu/anfangen.webp',
     'ausleihen':'https://sprachpilot.b-cdn.net/Neu/ausleihen.webp',
@@ -12,7 +13,43 @@
     'schliessen':'https://sprachpilot.b-cdn.net/Neu/schliessen.webp',
     'spazieren gehen':'https://sprachpilot.b-cdn.net/Neu/spazierengehen.webp',
     'spazierengehen':'https://sprachpilot.b-cdn.net/Neu/spazierengehen.webp',
-    'vereinbaren':'https://sprachpilot.b-cdn.net/Neu/vereinbaren.webp'
+    'vereinbaren':'https://sprachpilot.b-cdn.net/Neu/vereinbaren.webp',
+    'ausfallen':BASE+'ausfallen.webp',
+    'aufbacken':BASE+'aufbacken.webp',
+    'verschwenden':BASE+'verschwenden.webp',
+    'sich verändern':BASE+'sich_veraendern.webp',
+    'verwechseln':BASE+'verwechseln.webp',
+    'tauschen':BASE+'tauschen.webp',
+    'austauschen':BASE+'austauschen.webp',
+    'ablenken':BASE+'ablenken.webp',
+    'absagen':BASE+'absagen.webp',
+    'stören':BASE+'stoeren.webp',
+    'vermuten':BASE+'vermuten.webp',
+    'abraten':BASE+'abraten.webp',
+    'beraten':BASE+'beraten.webp',
+    'vorschlagen':BASE+'vorschlagen.webp',
+    'wählen':BASE+'waehlen.webp',
+    'entscheiden':BASE+'entscheiden.webp',
+    'aussuchen':BASE+'aussuchen.webp',
+    'ruinieren':BASE+'ruinieren.webp',
+    'leiten':BASE+'leiten.webp',
+    'sich benehmen':BASE+'sich_benehmen.webp',
+    'sich vorstellen':BASE+'sich_vorstellen.webp',
+    'sich kämmen':BASE+'sich_kaemmen.webp',
+    'sich rasieren':BASE+'sich_rasieren.webp',
+    'sich schminken':BASE+'sich_schminken.webp',
+    'sich bewegen':BASE+'sich_bewegen.webp',
+    'wandern':BASE+'wandern.webp',
+    'meinen':BASE+'meinen.webp',
+    'grillen':BASE+'grillen.webp',
+    'wecken':BASE+'wecken.webp',
+    'üben':BASE+'ueben.webp',
+    'trainieren':BASE+'trainieren.webp',
+    'losfahren':BASE+'losfahren.webp',
+    'dabeihaben':BASE+'dabeihaben.webp',
+    'leidtun':BASE+'leidtun.webp',
+    'leiden':BASE+'leiden.webp',
+    'opfern':BASE+'opfern.webp'
   };
   const oldImageBox=window.imageBox;
   const oldHydrate=window.hydrateImages;
@@ -45,4 +82,5 @@
     });
   };
   window.renderAndHydrate=function(){setTimeout(()=>window.hydrateImages(document),80)};
+  setTimeout(()=>window.hydrateImages(document),120);
 })();
