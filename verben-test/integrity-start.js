1c8ab49a9fe4264ab1459d7511639fe662b9bece
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-15 11:36:52 +0200

    Remove legacy Verben Test integrity start

--- verben-test/integrity-start.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const start=window.VT_START;
-  if(typeof start!=='function')throw new Error('Startfunktion für Verben Test fehlt.');
-  window.VT_START=async function(){
-    await start();
-    if(typeof state!=='undefined'&&state&&typeof assertIntegrity==='function')assertIntegrity();
-  };
-})();
