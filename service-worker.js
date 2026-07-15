d75899dda47f4b15fff64d549c36cfca6b8e12bb
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-15 11:34:22 +0200

    Refresh cache for clean Verben Test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="sprachpilot-static-v20260715-verben-ueben-button";
+const CACHE_VERSION="sprachpilot-static-v20260715-verben-test-clean";
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
