3d60cd7facb78e2be2abcf6ba037f28bcad13cce
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-17 14:43:44 +0200

    Lektion 7: maschinenlesbare Themen- und Aufgabenstruktur anlegen

---
+++ wortschatz/A1-Lektion-7/lektion7-struktur.js
@@ -0,0 +1,54 @@
+(function(){
+'use strict';
+window.L7_STRUCTURE={
+ lesson:'A1-Lektion-7',
+ level:'A1',
+ status:'planned',
+ globalRules:{
+  articlesAndPlurals:true,
+  repeatWrongAtEnd:true,
+  writingFallbackForSpeaking:true,
+  teacherPreviewScores:false,
+  passive:false,
+  closedTasksNeedOneAnswer:true,
+  solutionsRequired:true,
+  unclearWordsExcluded:['Direktometer']
+ },
+ themes:[
+  {
+   id:'Thema-1',
+   title:'können, wollen und möchten',
+   grammar:['können','wollen','Modalverb auf Position 2','Infinitiv am Satzende','Ja-/Nein-Fragen','W-Fragen','wollen oder möchten'],
+   taskOrder:[
+    'karteikarten.html','bild-erklaerung-wort.html','artikel-plural.html','koennen-formen.html','wollen-formen.html','verbform-waehlen.html','aussagen-ordnen.html','ja-nein-fragen.html','w-fragen.html','faehigkeiten-abstufen.html','bildimpulse.html','fragen-antworten.html','partnerinterview.html','wollen-möchten.html','dialoge-ergaenzen.html','hoeren-wuensche.html','eigene-faehigkeiten.html','eigene-plaene.html','pruefung.html'
+   ]
+  },
+  {
+   id:'Thema-2',
+   title:'Perfekt mit haben',
+   grammar:['haben im Perfekt','Partizip II','Endung -t','Endung -en','Partizip am Satzende','Zeitangaben'],
+   restrictions:['nur Perfekt mit haben','keine Bewegungsverben mit sein'],
+   taskOrder:[
+    'karteikarten.html','infinitiv-partizip.html','memory.html','endung-sortieren.html','endung-markieren.html','silben-ordnen.html','partizip-waehlen.html','partizip-schreiben.html','hoeren-partizip.html','fehler-korrigieren.html','haben-konjugieren.html','satzklammer.html','saetze-ordnen.html','saetze-bilden.html','zeitangaben.html','dialogluecken.html','fragen-antworten.html','lesen-tagesrueckblick.html','hoeren-rueckblick.html','eigene-saetze.html','pruefung.html'
+   ]
+  },
+  {
+   id:'Thema-3',
+   title:'Perfekt mit sein und Bewegungsverben',
+   grammar:['sein im Perfekt','gegangen','gefahren','gekommen','geflogen','gewandert','haben oder sein'],
+   restrictions:['zuerst nur Perfekt mit sein','haben/sein erst in der zweiten Phase'],
+   taskOrder:[
+    'karteikarten.html','infinitiv-partizip.html','sein-konjugieren.html','hilfsverb-sein.html','partizip-waehlen.html','saetze-ordnen.html','bild-satz.html','bildimpulse.html','saetze-bilden.html','ja-nein-fragen.html','w-fragen.html','dialoge.html','hoeren-bewegung.html','haben-sein-sortieren.html','hilfsverb-waehlen.html','hilfsverb-schreiben.html','gemischte-saetze.html','fehler-korrigieren.html','lesen-wochenende.html','hoeren-was-passiert.html','eigene-saetze.html','pruefung.html'
+   ]
+  },
+  {
+   id:'Thema-4',
+   title:'Kommunikation in der Schule',
+   grammar:['können in Mitteilungen','Perfekt als Wiederholung','Artikel','Singular und Plural'],
+   taskOrder:[
+    'karteikarten.html','artikel.html','plural-sprechen.html','wort-bedeutung.html','redemittel-ordnen.html','lesen-richtig-falsch.html','lesen-abc.html','informationen-markieren.html','ueberschrift.html','rechtschreibung.html','informationen-schreiben.html','hoeren-sekretariat.html','hoerdialog-ordnen.html','telefonluecken.html','telefonat-sprechen.html','dialog-deutschkurs.html','dialog-schulausflug.html','dialog-treffpunkt.html','entschuldigung-schule.html','nachricht-deutschkurs.html','entschuldigung-pruefen.html','eigener-dialog.html','pruefung.html'
+   ]
+  }
+ ]
+};
+})();
