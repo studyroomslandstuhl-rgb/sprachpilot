732a1e64e1bdd41aa4c0d64661ff2bbe878733f4
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-07-21 18:21:47 +0200

    Connect grouped verb translations and sentences

---
+++ verben/lexicon-bridge.js
@@ -0,0 +1,6 @@
+(function(){
+'use strict';
+window.SP_VERB_SENTENCES=Object.assign({},window.SP_VERB_SENTENCES||{},window.VERB_SENTENCES||{});
+window.VERB_TRANSLATIONS=window.VERB_TRANSLATIONS||{};
+window.VERB_TRANSLATIONS.Englisch=Object.assign({},window.SP_VERB_ENGLISH||{},window.VERB_TRANSLATIONS.Englisch||{});
+})();
