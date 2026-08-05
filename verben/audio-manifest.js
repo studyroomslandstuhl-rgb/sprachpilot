234e78425d5d2eba69cff34b31e342fc690a2762
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-05 22:55:53 +0200

    Add placeholder verb audio manifest

---
+++ verben/audio-manifest.js
@@ -0,0 +1,6 @@
+(function(){
+'use strict';
+window.SP_VERB_AUDIO_FILES=Object.freeze({});
+window.SP_VERB_AUDIO_MISSING=Object.freeze([]);
+window.SP_VERB_AUDIO_AUDIT=Object.freeze({generatedAt:null,total:0,available:0,missing:0,bases:[]});
+})();
