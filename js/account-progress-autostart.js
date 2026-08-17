0918116dbf336b1816b2db39510d21e32908294e
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-17 14:46:27 +0200

    Use safe account progress sync

--- js/account-progress-autostart.js
+++ js/account-progress-autostart.js
@@ -1,4 +1,8 @@
-import { startAccountProgressSync } from "/js/account-progress-sync.js?v=2";
+import { startAccountProgressSync } from "/js/account-progress-sync-safe.js?v=3";
+
+if(/^\/wortschatz\/?(?:index\.html)?$/i.test(location.pathname)){
+  import('/wortschatz/lesson-colors-pastel.js?v=2').catch(()=>{});
+}
 
 startAccountProgressSync().catch(error=>{
   console.warn("Account-Fortschritt Sync konnte nicht gestartet werden",error);
