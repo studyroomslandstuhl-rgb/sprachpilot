26a993303cc7947be5b9cbf8bc525933f39e9b9b
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-17 21:18:37 +0200

    Apply Wortschatz lesson colors before progress sync

--- js/account-progress-autostart.js
+++ js/account-progress-autostart.js
@@ -1,9 +1,9 @@
-import { startAccountProgressSync } from "/js/account-progress-sync.js?v=4";
-
 if(/^\/wortschatz\/?(?:index\.html)?$/i.test(location.pathname)){
-  import('/wortschatz/lesson-colors-pastel.js?v=3').catch(()=>{});
+  import('/wortschatz/lesson-colors-pastel.js?v=4').catch(()=>{});
 }
 
-startAccountProgressSync().catch(error=>{
-  console.warn("Account-Fortschritt Sync konnte nicht gestartet werden",error);
-});
+import('/js/account-progress-sync.js?v=4')
+  .then(mod=>mod.startAccountProgressSync?.())
+  .catch(error=>{
+    console.warn("Account-Fortschritt Sync konnte nicht gestartet werden",error);
+  });
