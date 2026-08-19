956cb386e5a8f07e0a49dd5a5cf42a4ab473f164
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-19 09:28:48 +0200

    L7T4: reliably load custom task renderers after shared L7 UI is ready

---
+++ wortschatz/A1-Lektion-7/Thema-4/l7t4-custom-ui-loader.js
@@ -0,0 +1,20 @@
+(function(){
+'use strict';
+if(window.__SP_L7T4_CUSTOM_UI_LOADER_V1)return;window.__SP_L7T4_CUSTOM_UI_LOADER_V1=true;
+let tries=0;
+function load(src){return new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.body.appendChild(s)})}
+function start(){
+ if(window.L7&&window.L7S){
+  load('/wortschatz/A1-Lektion-7/Thema-4/l7t4-custom-ui.js?v=2')
+   .then(()=>load('/wortschatz/A1-Lektion-7/Thema-4/l7t4-listening-ui.js?v=1'))
+   .then(()=>{
+    const id=new URLSearchParams(location.search).get('task');
+    if(document.body.dataset.page==='task'&&id)window.L7?.renderTaskPage?.(4,id)
+   })
+   .catch(e=>console.warn('L7T4 Zusatzoberfläche konnte nicht geladen werden',e));
+  return;
+ }
+ if(++tries<200)setTimeout(start,50);
+}
+start();
+})();
