557cd554edb527cdfb8b6c95309e759eabdcba4d
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-22 11:35:49 +0200

    L7T3: aktualisierte Aufgabe 17 UI laden

--- wortschatz/A1-Lektion-7/Thema-3/l7t3-boot.js
+++ wortschatz/A1-Lektion-7/Thema-3/l7t3-boot.js
@@ -28,7 +28,7 @@ Promise.resolve(window.L7_THEME_READY)
    .then(()=>load('l7t3-special-ui.js?v=2'))
    .then(()=>load('l7t3-help-standard.js?v=1'))
    .then(()=>load('../shared/l7-reading-queue-fix.js?v=2'))
-   .then(()=>load('l7t3-schon-einmal-ui.js?v=1'))
+   .then(()=>load('l7t3-schon-einmal-ui.js?v=2'))
    .then(()=>{
     window.SPL7WrongQueueV4?.install?.();window.L7T2MemoryUI?.install?.();window.L7T2EndingsUI?.install?.();window.L7T2AdvancedUI?.install?.();window.L7T3SpecialUI?.install?.();window.L7T3HelpStandard?.install?.();window.L7ReadingQueueFix?.install?.();window.L7T3SchonEinmalUI?.install?.();
     const result=window.L7.renderTaskPage(theme,new URLSearchParams(location.search).get('task'));window.L7T3CardImages?.patchAll?.(document);return result;
