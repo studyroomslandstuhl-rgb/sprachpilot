dff5d10170e6a3ca56e647bbb789fe00c09d8248
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-24 20:15:10 +0200

    Refresh registration entry for custom verification mail

--- js/student-register-entry-v2.js
+++ js/student-register-entry-v2.js
@@ -3,7 +3,7 @@ import {
   hasPendingStudentRegistration,
   $, safeText
 } from './student-identity.js?v=identity4';
-import { registerStudentOnce } from './student-registration-v3.js?v=20260824-register4';
+import { registerStudentOnce } from './student-registration-v3.js?v=20260824-register5';
 
 export { finishPendingStudentRegistration, hasPendingStudentRegistration, $, safeText };
 
