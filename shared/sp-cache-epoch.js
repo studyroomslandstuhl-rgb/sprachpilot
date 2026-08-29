fa42e55fabd5f0773fc1529c06f88625967a0535
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-29 17:56:08 +0200

    Bump cache epoch for durable point ledger

--- shared/sp-cache-epoch.js
+++ shared/sp-cache-epoch.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const EPOCH='20260829-points5';
+const EPOCH='20260829-points6';
 const KEY='SP_CACHE_EPOCH';
 if(window.__SP_CACHE_EPOCH_V4)return;window.__SP_CACHE_EPOCH_V4=true;
 async function clearAppCaches(){
