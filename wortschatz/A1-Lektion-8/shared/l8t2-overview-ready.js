b148e10991eb93f4c79fbabd557c98e134de8c25
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-31 07:26:15 +0200

    L8T2 Übersicht auf Übersetzungen und Struktur warten lassen

--- wortschatz/A1-Lektion-8/shared/l8t2-overview-ready.js
+++ wortschatz/A1-Lektion-8/shared/l8t2-overview-ready.js
@@ -1,7 +1,9 @@
 (function(){
 'use strict';
-if(window.__SP_L8T2_OVERVIEW_READY_V1)return;window.__SP_L8T2_OVERVIEW_READY_V1=true;
+if(window.__SP_L8T2_OVERVIEW_READY_V2)return;window.__SP_L8T2_OVERVIEW_READY_V2=true;
 const base=window.L8_CONTENT_READY;
 const final=window.L8_T2_CURRENT_READY;
-window.L8_CONTENT_READY=Promise.all([Promise.resolve(base),Promise.resolve(final)]).then(()=>window.L8_ALL_THEMES);
+const translations=window.L8_T2_TRANSLATIONS_READY;
+const config=window.L8_T2_OVERVIEW_CONFIG_READY;
+window.L8_CONTENT_READY=Promise.all([Promise.resolve(base),Promise.resolve(final),Promise.resolve(translations),Promise.resolve(config)]).then(()=>window.L8_ALL_THEMES);
 })();
