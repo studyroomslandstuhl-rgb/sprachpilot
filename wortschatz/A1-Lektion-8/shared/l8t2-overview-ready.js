e177c9af6203da42d2091cf3ebbd00f38f306e2f
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-31 07:50:00 +0200

    L8T2 Übersicht auf alle 13 Übersetzungssprachen warten lassen

--- wortschatz/A1-Lektion-8/shared/l8t2-overview-ready.js
+++ wortschatz/A1-Lektion-8/shared/l8t2-overview-ready.js
@@ -1,10 +1,11 @@
 (function(){
 'use strict';
-if(window.__SP_L8T2_OVERVIEW_READY_V3)return;window.__SP_L8T2_OVERVIEW_READY_V3=true;
+if(window.__SP_L8T2_OVERVIEW_READY_V4)return;window.__SP_L8T2_OVERVIEW_READY_V4=true;
 const base=window.L8_CONTENT_READY;
 const final=window.L8_T2_CURRENT_READY;
 const translations=window.L8_T2_TRANSLATIONS_READY;
 const vocab=window.L8_T2_VOCAB_READY;
+const extraTranslations=window.L8_T2_EXTRA_TRANSLATIONS_READY;
 const config=window.L8_T2_OVERVIEW_CONFIG_READY;
-window.L8_CONTENT_READY=Promise.all([Promise.resolve(base),Promise.resolve(final),Promise.resolve(translations),Promise.resolve(vocab),Promise.resolve(config)]).then(()=>window.L8_ALL_THEMES);
+window.L8_CONTENT_READY=Promise.all([Promise.resolve(base),Promise.resolve(final),Promise.resolve(translations),Promise.resolve(vocab),Promise.resolve(extraTranslations),Promise.resolve(config)]).then(()=>window.L8_ALL_THEMES);
 })();
