54e11c838928c27ed16b31fdcda7c9a6e8e160a3
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-31 08:36:03 +0200

    Autostart auf zentralen Sync-Stand central2 anheben

--- js/account-progress-autostart.js
+++ js/account-progress-autostart.js
@@ -1,12 +1,12 @@
 import '/js/session-restore.js?v=5';
 import '/shared/points-recalculator.js?v=2';
-import '/shared/dativ-points-extension.js?v=2';
+import '/shared/dativ-points-extension.js?v=4';
 
 if(/^\/wortschatz\/?(?:index\.html)?$/i.test(location.pathname))import('/wortschatz/lesson-colors-pastel.js?v=4').catch(()=>{});
 
 // Ein Einstieg für den Kontofortschritt. Keine zusätzliche automatische Punkte-Reparatur
 // beim Seitenstart; neue Punkte werden transaktional in progress.js vergeben und alte
 // Teilnehmerstände nur gezielt repariert.
-import('/js/account-progress-sync.js?v=20260831-central1')
+import('/js/account-progress-sync.js?v=20260831-central2')
   .then(mod=>mod.startAccountProgressSync?.())
   .catch(error=>console.warn('Account-Fortschritt Sync konnte nicht gestartet werden',error));
