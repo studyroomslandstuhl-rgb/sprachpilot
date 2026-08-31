dbfb8992b8611dae3d8d4131bd9b8fa053c260df
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-31 07:48:04 +0200

    L8T2 Aufgaben erst nach finalem Wortschatz starten

--- wortschatz/A1-Lektion-8/shared/l8t2-boot-after-translations.js
+++ wortschatz/A1-Lektion-8/shared/l8t2-boot-after-translations.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-if(window.__SP_L8T2_BOOT_AFTER_TRANSLATIONS_V1)return;window.__SP_L8T2_BOOT_AFTER_TRANSLATIONS_V1=true;
-Promise.resolve(window.L8_T2_TRANSLATIONS_READY||window.L8_T2_CURRENT_READY||window.L8_CONTENT_READY).catch(()=>{}).finally(()=>{
- const script=document.createElement('script');script.src='../shared/l8-boot.js?v=20260831-t2translations1';document.body.appendChild(script);
+if(window.__SP_L8T2_BOOT_AFTER_TRANSLATIONS_V2)return;window.__SP_L8T2_BOOT_AFTER_TRANSLATIONS_V2=true;
+Promise.resolve(window.L8_T2_VOCAB_READY||window.L8_T2_TRANSLATIONS_READY||window.L8_T2_CURRENT_READY||window.L8_CONTENT_READY).catch(()=>{}).finally(()=>{
+ const script=document.createElement('script');script.src='../shared/l8-boot.js?v=20260831-t2vocab1';document.body.appendChild(script);
 });
 })();
