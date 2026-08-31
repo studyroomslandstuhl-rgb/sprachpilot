f9ba758ab66c8abaa220ab6d561e9d6da8ea8ecc
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-08-31 09:14:06 +0200

    Bump cache epoch for sync and device merge fixes

--- shared/sp-cache-epoch.js
+++ shared/sp-cache-epoch.js
@@ -1,8 +1,8 @@
 (function(){
 'use strict';
-const EPOCH='20260829-points11-individual';
+const EPOCH='20260831-sync-device-merge1';
 const KEY='SP_CACHE_EPOCH';
-if(window.__SP_CACHE_EPOCH_V8)return;window.__SP_CACHE_EPOCH_V8=true;
+if(window.__SP_CACHE_EPOCH_V9)return;window.__SP_CACHE_EPOCH_V9=true;
 async function clearAppCaches(){
  const jobs=[];
  try{if('caches'in window)jobs.push(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))))}catch(e){}
