8aa183ad30031ae953c6a309823056da81e54f83
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-09-02 12:26:02 +0200

    Restore pre-performance L8T2 boot wrapper

---
+++ wortschatz/A1-Lektion-8/Thema-2/l8t2-boot-preperf.js
@@ -0,0 +1,10 @@
+(function(){
+'use strict';
+if(window.__SP_L8T2_BOOT_PREPERF)return;
+window.__SP_L8T2_BOOT_PREPERF=true;
+Promise.resolve(window.L8_T2_TASK3_SEIT_VOR_READY||window.L8_T2_TASK2_PLURAL_READY||window.L8_T2_VOCAB_FINAL_READY||window.L8_T2_MEDIA_FIXES_READY||window.L8_T2_EXTRA_TRANSLATIONS_READY||window.L8_T2_VOCAB_READY||window.L8_T2_TRANSLATIONS_READY||window.L8_T2_CURRENT_READY||window.L8_CONTENT_READY).catch(()=>{}).finally(()=>{
+ const script=document.createElement('script');
+ script.src='l8t2-core-boot-preperf.js?v=20260902-preperf1';
+ document.body.appendChild(script);
+});
+})();
