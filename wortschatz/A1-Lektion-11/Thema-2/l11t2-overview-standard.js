ac39f334778fae11c662f2418a72b6e80611c1f3
studyroomslandstuhl-rgb <studyroomslandstuhl@gmail.com>
2026-09-16 12:30:16 +0200

    Add L11T2 standard overview adapter

---
+++ wortschatz/A1-Lektion-11/Thema-2/l11t2-overview-standard.js
@@ -0,0 +1,7 @@
+(function(){'use strict';
+const D=window.L11T2||{cards:[]};
+const items=(D.cards||[]).map(w=>({id:w.id,term:w.full,full:w.full,plural:w.plural||'',type:'noun',image:w.image,audio:w.audio,audioFile:(w.audio||'').split('/').pop()||'',translations:w.translations||{}}));
+window.L8_THEME={number:2,title:'Unterwegs mit Verkehrsmitteln',tasks:[{id:'karteikarten',kind:'cards',title:'Karteikarten',items}]};
+window.L8_OVERVIEW_CONTEXT={lesson:11,subtitle:'Wortschatzübersicht · Unterwegs mit Verkehrsmitteln · A1 Lektion 11 · Thema 2'};
+window.L8_CONTENT_READY=Promise.resolve(window.L8_THEME);
+})();
